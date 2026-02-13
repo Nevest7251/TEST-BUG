76c8379ed03b497362de4642c9c416e208494a3c
Nevest7251 <kruzz474@gmail.com>
2026-02-13 07:20:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-<p>hallo Guys</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Jam</title>
+</head>
+<body>
+  <div class = "clock" id="clock">00:00:00</div>
+</body>
+<script>
+  function updateJam() {
+    const now = new Date()
+    
+    const jam = String(now.getHours()).padStart(2,'0')
+    const menit = String(now.getMinutes()).padStart(2,'0')
+    const detik = String(now.getSeconds()).padStart(2,'0')
+    
+    document.getElementById('clock').textContent=`${jam}:${menit}:${detik}`;
+  }
+  setInterval(updateJam,1000);
+  updateJam()
+</script>
+</html>
